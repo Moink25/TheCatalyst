a0235e2732320d75fb1c44d6ac839aed2a281a47
Moink25 <khanmoin9570@gmail.com>
2024-12-05 19:14:31 +0530

    5 dec 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
     }, 3000);
     return () => clearInterval(interval); // Clean up interval on component unmount
-  }, []);
+  }, );
 
   const bgRef = useRef();
 
